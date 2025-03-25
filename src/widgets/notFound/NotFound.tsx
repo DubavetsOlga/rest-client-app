4b45679e293dee4523c76d80a4d1bf98f786f5c6
Olga Dubavets <o.dubavets@gmail.com>
2025-03-25 15:54:05 +0100

    feat: add not found page

---
+++ src/widgets/notFound/NotFound.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import s from './style.module.css';
+
+type Props = {
+  text?: string;
+  linkText?: string;
+};
+
+export const NotFound = ({
+  text = 'Page Not Found',
+  linkText = 'Go to Main',
+}: Props) => {
+  return (
+    <div className={s.container}>
+      <h1 className={s.title}>404</h1>
+      <h2 className={s.subtitle}>{text}</h2>
+      <Link href="/" className={s.link}>
+        {linkText}
+      </Link>
+    </div>
+  );
+};
